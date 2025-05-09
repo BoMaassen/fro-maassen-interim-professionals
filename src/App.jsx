1ff322761512faca4b299c0d322ef06f82e7993a
BoMaassen <bomaassen@live.nl>
2025-05-09 16:52:23 +0200

    routing toegevoegd en homepage aangemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './pages/home/home'
 
 function App() {
 
@@ -6,6 +8,9 @@ function App() {
     <>
       <h1>Hi</h1>
       <p>test</p>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </>
   )
 }
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,7 @@
+function Home(){
+    return( <main>
+    <h2>Homepage</h2>
+    </main>
+    )
+}
+export default Home;
