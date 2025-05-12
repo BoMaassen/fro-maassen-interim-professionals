6b796ad5756b9f3a3133f84d15716490220f518c
BoMaassen <bomaassen@live.nl>
2025-05-12 17:24:47 +0200

    header gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import './App.css'
-
 import Navbar from './components/navbar/navbar'
 import About from './pages/about/About'
 import Assignments from './pages/assignments/Assignments'
@@ -8,13 +7,24 @@ import Home from './pages/home/home'
 import Contact from './pages/contact/Contact'
 
 function App() {
+  const location = useLocation();
+
+  const titles = {
+    '/': 'Interim opdrachten voor professionals',
+    '/over-ons': 'Over ons',
+    '/contact': 'Contact ons',
+    '/opdrachten': 'Opdrachten',
+  };
+  
+  const getPageTitle = () => titles[location.pathname] || 'Pagina';
 
   return (
     <>
     <header>
       <Navbar/>
-      <h1>Hi</h1>
-      <p>test</p>
+      <div className='header-content'>
+      <h1>{getPageTitle()}</h1>
+      </div>
     </header>
      
       <Routes>
