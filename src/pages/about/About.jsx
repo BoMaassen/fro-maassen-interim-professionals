14b4264191f36159743dfe9981c8d8aeedf7e99a
BoMaassen <bomaassen@live.nl>
2025-07-18 12:42:40 +0200

    about-us section

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,8 +1,18 @@
 import './About.css'
+import logoMip from "../../assets/LOGO_MIP.png";
 
 function About(){
     return( <main>
-    <h2>Over ons</h2>
+<section className="about-us">
+    <span>
+<img src={logoMip} alt="Logo Maassen interim professionals"/>
+    </span>
+    <div className='about-text'>
+<h2> Wie we zijn</h2>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus eum id pariatur facere, nulla quidem nihil fugit magni officia quisquam vero nam, cum dolor earum. Numquam praesentium hic a est, impedit totam in, recusandae, voluptatem pariatur soluta ad dolor voluptate eaque debitis porro odit vero. Doloremque repudiandae blanditiis odit eligendi!
+</p>
+    </div>
+</section>
     </main>
     )
 }
