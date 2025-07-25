8021ec2fbf0a3aacb11e4688e3307d18786f7be3
BoMaassen <bomaassen@live.nl>
2025-07-25 14:17:51 +0200

    opzet contact card af

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,26 @@
 import './Contact.css'
+import {PhoneIcon, EnvelopeIcon } from "@phosphor-icons/react";
+import fotoFrank from "../../assets/Frank.jpeg";
 
 function Contact(){
     return( <main>
-    <h2>Contact</h2>
+    <section>
+        <div className='contact-card'>
+          <span><img src={fotoFrank} alt='Frank Maassen'/></span>  
+          <div>
+            <h4>Frank Maassen</h4>
+            <div>
+                <PhoneIcon size={12} />
+                <h5>06-41000136</h5>
+            </div>
+            <div>
+                <EnvelopeIcon size={12} />
+                <h5>info@maasseninterimprofessionals.nl</h5>
+            </div>
+          </div>
+
+        </div>
+    </section>
     </main>
     )
 }
