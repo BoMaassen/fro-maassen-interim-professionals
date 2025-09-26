2463f6f99d3276a20c3e7eed628cdb83624b0905
BoMaassen <bomaassen@live.nl>
2025-09-26 16:25:30 +0200

    assignments opgehaald met json file

--- src/pages/assignments/Assignments.jsx
+++ src/pages/assignments/Assignments.jsx
@@ -1,33 +1,46 @@
 import './Assignments.css'
 import Assignment from "../../components/assignment/Assignment";
+import assignments from "../../../src/assignments.json"
 
-function Assignments(){
-    return( <main>
-    <section className='assignments-section'>
-        <div className='filter'>
-            <div className='filtermenu'>
-                <h2>Filter</h2>
-                <h4>Plaats</h4>
-                <ul>
-                    <li>Arnhem</li>
-                    <li>Nijmegen</li>
-                    <li>Utrecht</li>
-                </ul>
-                <h4>Uren</h4>
-                <h4>Opleidingsniveau</h4>
+function Assignments() {
+    return (<main>
+        <section className='assignments-section'>
+            <div className='filter'>
+                <div className='filtermenu'>
+                    <h2>Filter</h2>
+                    <details>
+                        <summary>Plaats</summary>
+                        <ul>
+                            <li>Arnhem</li>
+                            <li>Nijmegen</li>
+                            <li>Utrecht</li>
+                        </ul>
+                    </details>
+                    <details>
+                        <summary>Uren</summary>
+                        <ul>
+                            <li>24</li>
+                            <li>36</li>
+                            <li>40</li>
+                        </ul>
+                    </details>
+                    <details>
+                        <summary>Opleidingsniveau</summary>
+                        <ul>
+                            <li>Hbo</li>
+                            <li>Wo</li>
+                            <li>Mbo</li>
+                        </ul>
+                    </details>
+                </div>
             </div>
-        </div>
-        <div className='assignments-div'>
-        <Assignment title="Interim manager" place="Arnhem" hours="32-26" salary="75-90" education="Hbo" buttoncolor="blue" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quisquam rem, accusantium qui sunt illo blanditiis suscipit similique quo eveniet laboriosam incidunt?..."/>
-        <Assignment title="Interim manager" place="Arnhem" hours="32-26" salary="75-90" education="Hbo" buttoncolor="black" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quisquam rem, accusantium qui sunt illo blanditiis suscipit similique quo eveniet laboriosam incidunt?..."/>
-        <Assignment title="Interim manager" place="Arnhem" hours="32-26" salary="75-90" education="Hbo" buttoncolor="purple" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quisquam rem, accusantium qui sunt illo blanditiis suscipit similique quo eveniet laboriosam incidunt?..."/>
-        <Assignment title="Interim manager" place="Arnhem" hours="32-26" salary="75-90" education="Hbo" buttoncolor="red" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quisquam rem, accusantium qui sunt illo blanditiis suscipit similique quo eveniet laboriosam incidunt?..."/>
-        <Assignment title="Interim manager" place="Arnhem" hours="32-26" salary="75-90" education="Hbo" buttoncolor="blue" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quisquam rem, accusantium qui sunt illo blanditiis suscipit similique quo eveniet laboriosam incidunt?..."/>
-        <Assignment title="Interim manager" place="Arnhem" hours="32-26" salary="75-90" education="Hbo" buttoncolor="black" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quisquam rem, accusantium qui sunt illo blanditiis suscipit similique quo eveniet laboriosam incidunt?..."/>
-        <Assignment title="Interim manager" place="Arnhem" hours="32-26" salary="75-90" education="Hbo" buttoncolor="purple" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quisquam rem, accusantium qui sunt illo blanditiis suscipit similique quo eveniet laboriosam incidunt?..."/>
-        <Assignment title="Interim manager" place="Arnhem" hours="32-26" salary="75-90" education="Hbo" buttoncolor="red" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quisquam rem, accusantium qui sunt illo blanditiis suscipit similique quo eveniet laboriosam incidunt?..."/>
-        </div> 
-    </section>
+            <div className='assignments-div'> {assignments.map((assignment => {
+                return (
+                    <Assignment title={assignment.title} place={assignment.place} hours={assignment.hours} salary={assignment.salary} education={assignment.education} buttoncolor={assignment.buttoncolor} text={assignment.text} />
+                )
+            }))}
+            </div>
+        </section>
     </main>
     )
 }
