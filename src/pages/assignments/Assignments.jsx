2d10cdef0c8d683737028324ffb0c9df18482c5e
BoMaassen <bomaassen@live.nl>
2025-10-25 13:26:34 +0200

    plaatsen menu gefilterd

--- src/pages/assignments/Assignments.jsx
+++ src/pages/assignments/Assignments.jsx
@@ -1,8 +1,14 @@
 import './Assignments.css'
 import Assignment from "../../components/assignment/Assignment";
 import assignments from "../../../src/assignments.json"
+import { useState } from 'react';
 
 function Assignments() {
+    const places = assignments.map((assignment) =>  assignment.place);
+    const uniquePlaces = places.filter((place, id, array) => array.indexOf(place) == id);
+    const sortedPlaces = uniquePlaces.sort((a, b) => a.localeCompare(b));
+    console.log(sortedPlaces);
+
     return (<main>
         <section className='assignments-section'>
             <div className='filter'>
@@ -11,32 +17,28 @@ function Assignments() {
                     <details>
                         <summary>Plaats</summary>
                         <ul>
-                            <li>Arnhem</li>
-                            <li>Nijmegen</li>
-                            <li>Utrecht</li>
+                            {sortedPlaces.map((place) =>(
+                            <li key={place}>{place}</li>
+                            ))}      
                         </ul>
                     </details>
                     <details>
                         <summary>Uren</summary>
                         <ul>
-                            <li>24</li>
-                            <li>36</li>
-                            <li>40</li>
+                            
                         </ul>
                     </details>
                     <details>
                         <summary>Opleidingsniveau</summary>
                         <ul>
-                            <li>Hbo</li>
-                            <li>Wo</li>
-                            <li>Mbo</li>
+                            
                         </ul>
                     </details>
                 </div>
             </div>
             <div className='assignments-div'> {assignments.map((assignment => {
                 return (
-                    <Assignment title={assignment.title} place={assignment.place} hours={assignment.hours} salary={assignment.salary} education={assignment.education} buttoncolor={assignment.buttoncolor} text={assignment.text} />
+                    <Assignment key={assignment.id} title={assignment.title} place={assignment.place} hours={assignment.hours} salary={assignment.salary} education={assignment.education} buttoncolor={assignment.buttoncolor} text={assignment.text} />
                 )
             }))}
             </div>
