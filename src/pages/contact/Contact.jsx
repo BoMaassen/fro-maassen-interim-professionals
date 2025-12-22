9dda6b585d094705eb34dd011909ea869235053c
BoMaassen <bomaassen@live.nl>
2025-12-22 16:28:26 +0100

    formulier toegevoegd

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -20,7 +20,7 @@ function Contact() {
         if (!token) return; // gebruiker heeft reCAPTCHA niet voltooid
         // Hier kun je nu echt de data versturen
         console.log("Formulier verzonden met token:", token);
-      }
+    }
 
     return (<main>
         <section className='contact-section'>
@@ -38,40 +38,48 @@ function Contact() {
                     </div>
                 </div>
             </div>
-          
+
             <form onSubmit={handleSubmit(handleFormSubmit)}>
-            <h2>Neem contact op</h2>
+                <h2>Neem contact op</h2>
                 <div className='name-form'>
                     <Input inputType="text" inputName="firstname" placeholder="Voornaam" register={register} error={errors}
-                    validationRules={{required: {
-                            value: true,
-                            message: "Voornaam is verplicht",
-                        }}} />
+                        validationRules={{
+                            required: {
+                                value: true,
+                                message: "Voornaam is verplicht",
+                            }
+                        }} />
                     <Input inputType="text" inputName="lastname" placeholder="Achternaam" register={register} error={errors}
-                    validationRules={{required: {
-                            value: true,
-                            message: "Achternaam is verplicht",
-                        }}} />
+                        validationRules={{
+                            required: {
+                                value: true,
+                                message: "Achternaam is verplicht",
+                            }
+                        }} />
                 </div>
                 <div className='contacts-form'>
-                <Input inputType="text" inputName="email" placeholder="Email" register={register} error={errors}
-                    validationRules={{required: {
-                            value: true,
-                            message: "Email is verplicht",
-                        }}} />
-                <Input inputType="text" inputName="tel" placeholder="Telefoonnummer" register={register} error={errors}
-                    validationRules={{required: {
-                            value: true,
-                            message: "Telefoonnummer is verplicht",
-                        }}} />
+                    <Input inputType="text" inputName="email" placeholder="Email" register={register} error={errors}
+                        validationRules={{
+                            required: {
+                                value: true,
+                                message: "Email is verplicht",
+                            }
+                        }} />
+                    <Input inputType="text" inputName="tel" placeholder="Telefoonnummer" register={register} error={errors}
+                        validationRules={{
+                            required: {
+                                value: true,
+                                message: "Telefoonnummer is verplicht",
+                            }
+                        }} />
                 </div>
                 <label htmlFor='message'>
                     <textarea id='message' placeholder='Bericht' {...register("message")} />
                 </label>
-             <ReCAPTCHA ref={recaptchaRef} size="invisible" onChange={onReCAPTCHAChange}
-                sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" // test key
+                <ReCAPTCHA ref={recaptchaRef} size="invisible" onChange={onReCAPTCHAChange}
+                    sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" // test key
                 />
-                <Button className="text-button purple" text="versturen" type="submit"/>
+                <Button className="text-button purple" text="versturen" type="submit" />
             </form>
         </section>
     </main>
