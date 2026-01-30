d9411385a81d090a223681d620f760228873a482
BoMaassen <bomaassen@live.nl>
2026-01-30 16:03:59 +0100

    footer component gemaakt met klikbare links

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import About from './pages/about/About'
 import Assignments from './pages/assignments/Assignments'
 import Home from './pages/home/home'
 import Contact from './pages/contact/Contact'
-import logoMip from "./assets/LOGO_MIP.png";
-import { Link } from 'react-router-dom'
 import Vacancy from './pages/vacancy/Vacancy.JSX'
+import Footer from './components/footer/Footer';
+import ScrollToTop from './components/scrollToTop/scrollToTop';
 
 function App() {
   const location = useLocation();
@@ -38,7 +38,7 @@ function App() {
       <h1>{getPageTitle()}</h1>
       </div>
     </header>
-     
+    <ScrollToTop />
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/over-ons' element={<About/>}/>
@@ -46,42 +46,7 @@ function App() {
         <Route path='/opdrachten' element={<Assignments/>}/>
         <Route path='/opdracht/:id' element={<Vacancy setPageTitle={setPageTitle} />} />
       </Routes>
-      <div>
-      <footer>
-       <span className="footer-logo"><img src={logoMip} alt="Logo Maassen interim professionals"/></span>
-       <div>
-        <h4><Link to="/">Maassen interim professionals</Link></h4>
-        <ul>
-        <li>Kvk 123456789</li>
-        <li>Molenhoekplein 43</li>
-        <li>6675BZ Valburg</li>
-        </ul>
-       </div>
-       <div>
-        <h4><Link to="/over-ons">Over ons</Link></h4>
-        <ul>
-          <li>Wie we zijn</li>
-          <li>Wat we doen</li>
-        </ul>
-       </div>
-       <div>
-        <h4><Link to="/opdrachten">Opdrachten</Link></h4>
-        <ul>
-        <li>Interim manager 1</li>
-        <li>Interim manager 2</li>
-        <li>Interim manager 3</li>
-        </ul>
-       </div>
-       <div>
-        <h4><Link to="/contact">Contact</Link></h4>
-        <ul>
-        <li>06-41000136</li>
-        <li>info@maasseninterim<br></br>professionals.nl</li>
-        </ul>
-       </div>
-      </footer>
-      <div className="footnote"><p>@2025 - Maassen interim professionals</p></div>
-      </div>
+      <Footer/>
     </>
   )
 }
